9c4a0f91b0dd4d6592a3ced56e5a2f8067f6146b
RT007 <ravitiwari1989@gmail.com>
2024-10-12 05:42:45 +1100

    Update sscanf_fixed.c

--- Chapter10/sscanf_fixed.c
+++ Chapter10/sscanf_fixed.c
@@ -1,8 +1,14 @@
-int main() {
-	char* data = "John";
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	if (argc < 2)
+	{
+		printf("ERROR: Expected commandline args");
+		return -1;
+	}
 	char name[20];
-	int age;
-	int return_value = sscanf(data, "%s %i", name, &age);
+	int age = atoi(argv[2]); 
+	int return_value = sscanf(argv[1], "%s %i", name, &age);
 	if(return_value == 2){
 		printf("I'm %s.\n", name);
 		printf("I'm %i years old.", age);
@@ -11,4 +17,5 @@ int main() {
 	}else{
 		printf("ERROR: 2 values expected, %d given.\n", return_value);
 	}
+	return 0;
 }
